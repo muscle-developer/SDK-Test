aa4fe5672afcd6c821be31c896fbfbead11d18f9
muscle-developer <sintyoung5683@naver.com>
2025-06-02 20:07:54 +0900

    스크립트 생성 및 코드 수정

--- Assets/Addressables Test/Scripts/AddressableManager.cs
+++ Assets/Addressables Test/Scripts/AddressableManager.cs
@@ -1,16 +1,65 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AddressableAssets;   // 어드레서블을 사용하기 위한 네임스페이스
 
 public class AddressableManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    private AssetReferenceGameObject capsuleObj;
+    [SerializeField]
+    private AssetReferenceGameObject cubeObj;
+    [SerializeField]
+    private AssetReferenceGameObject sphereObj;
+
+    // 생성된 오브젝트들을 저장하는 리스트
+    private List<GameObject> gameObjs = new List<GameObject>();
+
     void Start()
     {
-        
+        StartCoroutine(InitAddressable());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator InitAddressable()
     {
-        
+        var init = Addressables.InitializeAsync();
+        yield return init;
+    }
+
+    // Load
+    public void ButtonSpawnObject() 
+    {
+        capsuleObj.InstantiateAsync().Completed += (obj) =>
+        {
+            gameObjs.Add(obj.Result);
+        };
+
+        cubeObj.InstantiateAsync().Completed += (obj) =>
+        {
+            gameObjs.Add(obj.Result);
+        };
+
+        sphereObj.InstantiateAsync().Completed += (obj) =>
+        {
+            gameObjs.Add(obj.Result);
+        };
+    }
+    
+    // Release
+    public void ButtonReleaseObject()
+    {
+        if (gameObjs.Count == 0)
+        {
+            return;
+        }
+
+        // coinSprite.ReleaseAsset();
+        // image.sprite = null;
+
+        for (int i = gameObjs.Count - 1; i >= 0; i--)
+        {
+            Addressables.ReleaseInstance(gameObjs[i]);
+            gameObjs.RemoveAt(i);
+        }
     }
 }
